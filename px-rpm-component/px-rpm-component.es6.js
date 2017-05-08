c97f474919a62638b50faabb50bcc071f4bc1051
Ryan McCormack <ryan.mccormack@ge.com>
2017-05-08 14:01:29 -0700

    initial gh-pages commit

---
+++ px-rpm-component/px-rpm-component.es6.js
@@ -0,0 +1,57 @@
+(function() {
+  Polymer({
+
+    is: 'px-rpm-component', 
+
+    properties: {
+      /**
+      * This property keeps track of the number of clicks.
+      *
+      * @property counterValue
+      */
+      boxSize: {
+        type: Number,
+        value: 50,
+        notify: true
+      },
+      world: {
+        type: String,
+        value: '',
+        notify: true
+      },
+      world_static: {
+        type: String,
+        value: '',
+        notify: true
+      }
+    },
+
+    /**
+    * Handles click on the link element defined in 'on-click' on the template.
+    *
+    * @method handleClick
+    */
+    handleClick: function(event, detail, sender) {
+      this.increment();
+    },
+
+    /**
+    * Handles click on the button element
+    *
+    * @method btnClick
+    */
+    // btnClick: function(event, detail, sender) {
+    //   console.log('You clicked the button...');
+    // },
+
+    /**
+    * Updates static text when button clicked
+    *
+    * @method updateStatic
+    */
+
+    updateStatic: function() {
+      this.world_static = 'Hello, ' + this.world + '!';
+    }
+  });
+})();
